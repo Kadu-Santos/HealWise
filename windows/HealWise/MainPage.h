6a4ef4a2c8baf663946f2faf9471dceb379d20df
Kadu-Santos <khadusantosed@gmail.com>
2023-07-13 17:56:20 -0300

    Carlos - Initial Commit

---
+++ windows/HealWise/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::HealWise::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::HealWise::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
